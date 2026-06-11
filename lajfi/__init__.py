29ebffd7e2438321881fef23bd5fc623d2125596
Nadeschda Barenje <nadeschda@vetroal.se>
2026-06-11 12:21:11 +0200

    Fix author name

--- lajfi/__init__.py
+++ lajfi/__init__.py
@@ -3,10 +3,10 @@ LAJFI - Evolutionary Life Simulation for Blender
 =================================================
 Procedural creature generation using Gielis superformulas.
 
-Author: Nadeschda Barenje Lindgren
+Author: Nadeschda Barenje
 License: MIT
 Repository: https://github.com/jinkieyz/lajfi
 """
 
 __version__ = "1.0.0"
-__author__ = "Nadeschda Barenje Lindgren"
+__author__ = "Nadeschda Barenje"
